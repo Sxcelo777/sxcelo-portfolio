1b2e4c84a19592353bf712b795f284c40c80260e
Sicelo Mathenjwa <85649694+Sxcelo777@users.noreply.github.com>
2026-09-02 18:26:54 +0200

    Update designs.js

--- src/data/designs.js
+++ src/data/designs.js
@@ -3,5 +3,5 @@
 export const designs = [
   { id: 'logo-01', title: 'Logo Concept', category: 'Branding', image: '/images/design-placeholder-1.png' },
   { id: 'poster-01', title: 'Company Logo', category: 'Logos', image: '/images/gongo-logo.png' },
-  { id: 'product-01', title: 'Product Mockup', category: 'Product Design', image: 'public/images/Capture.png' },
+  { id: 'product-01', title: 'Product Mockup', category: 'Product Design', image: '/images/Capture.png' },
 ]
